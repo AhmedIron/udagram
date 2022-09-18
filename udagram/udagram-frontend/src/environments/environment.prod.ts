41f763142da2687c83bd917fb9bc4683d734d037
AhmedIron <ahmediron2810@gmail.com>
2022-09-18 03:40:59 +0200

    init

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram-api",
-  apiHost: "http://Udagramapi-env.eba-z9pwtkyi.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: 'http://Udagramapi-env.eba-z9pwtkyi.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
