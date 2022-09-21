06f267add57d634585290d4c7db757747a488a3c
AhmedIron <ahmediron2810@gmail.com>
2022-09-21 07:16:11 +0200

    INIT

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,2 +1,15 @@
-aws s3 cp --recursive --acl public-read ./www s3://ahmedkhedr2810/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://ahmedkhedr2810/
+            eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+            AWS_BUCKET=$AWS_BUCKET
+            AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+            AWS_PROFILE=$AWS_PROFILE
+            AWS_REGION=$AWS_REGION
+            AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+            DB_PORT=$DB_PORT
+            JWT_SECRET=$JWT_SECRET
+            PORT=$PORT
+            POSTGRES_DB=$POSTGRES_DB
+            POSTGRES_HOST=$POSTGRES_HOST
+            POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+            POSTGRES_USERNAME=$POSTGRES_USERNAME
+            RDSDIALECT=$RDSDIALECT
+            URL=$URL
